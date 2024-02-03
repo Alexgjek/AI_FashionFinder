c48b03c68cc09af24b09653aaa610ebf2974d282
alexgjeka <alexgjeka0@gmail.com>
2024-02-02 23:29:54 -0500

    Created state variable user which will hold user profile information (email, pw, firstname, lastname). Added link that can redirect user to the login page.

--- fashionFinder-frontend/src/app/register/page.js
+++ fashionFinder-frontend/src/app/register/page.js
@@ -1,23 +1,69 @@
+'use client';
+
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+import RegisterButton from "@/components/buttons/RegisterButton";
+
 export default function RegisterPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+  });
+
+  const onSignup = async () => {
+  };
+
   return (
-    <main className="flex justify-center items-center">
-      <div className="flex flex-col items-center">
-        <h1 className="p-10 text-xl font-bold">Create your personal account</h1>
-        <div className="flex gap-3">
-        <input 
+    <div className="flex flex-col items-center justify-center py-10">
+      <h1 className="text-xl font-bold mb-5">
+        Create your personal account
+      </h1>
+      <hr />
+      <div className='flex flex-col'>
+        <input
+          className="border border-black p-3 outline-none mb-4"
+          id="email"
           type="text"
-          placeholder="First name *"
-          required
-          className="border border-black outline-none p-4"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="E-mail address"
         />
-        <input 
-          type="text"
-          placeholder="Last name *"
-          required
-          className="border border-black outline-none p-4"
-        />
+        <div className="flex gap-2">
+          <input
+            className="border border-black p-3 outline-none mb-4"
+            id="firstName"
+            type="text"
+            value={user.firstName}
+            onChange={(e) => setUser({ ...user, firstName: e.target.value })}
+            placeholder="First Name"
+          />
+          <input
+            className="border border-black p-3 outline-none mb-4"
+            id="lastName"
+            type="text"
+            value={user.lastName}
+            onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+            placeholder="Last Name"
+          />
         </div>
+        <input
+          className="border border-black p-3 outline-none mb-4"
+          id="password"
+          type="text"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="Password"
+        />
+        <RegisterButton onSignup={onSignup} />
       </div>
-    </main>
+      <div className="flex gap-1 p-2 text-sm">
+        <span>Already have an account?</span>
+        <Link href="/login" className="hover:underline hover:text-purple-800">Sign in</Link>
+      </div>
+    </div>
   );
 }
