d712fa2dc793a7db44cc3117269a29579fda4877
alexgjeka <alexgjeka0@gmail.com>
2024-02-02 23:34:49 -0500

    Updated the NextButton function to take in onLogin as a prop, displayed on login page

--- fashionFinder-frontend/src/components/buttons/SignInPageButton.js
+++ fashionFinder-frontend/src/components/buttons/SignInPageButton.js
@@ -1,13 +1,11 @@
 'use client';
 
-export default function NextButton() {
+export default function NextButton({ onLogin }) {
   return (
     <button 
-    onClick={()=> {
-      console.log("works");
-    }}
-    className="bg-black text-white py-4 px-3 mt-4 mb-4 w-full font-semibold text-center">
-       <span>NEXT</span>
+    className="bg-black text-white py-4 px-3 mt-4 mb-4 w-full font-semibold text-center"
+    onClick={onLogin}>
+       NEXT
     </button>
   );
 }
