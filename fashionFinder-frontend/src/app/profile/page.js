2510dad99401632f6d030deb4a8e9a6d613e60d8
alexgjeka <alexgjeka0@gmail.com>
2024-02-04 15:32:52 -0500

    Added logout button, logs out users and removes token from cookies

--- fashionFinder-frontend/src/app/profile/page.js
+++ fashionFinder-frontend/src/app/profile/page.js
@@ -1,4 +1,20 @@
+'use client'
+import axios from 'axios';
+import Link from 'next/link';
+import {useRouter} from 'next/navigation';
 export default function ProfilePage() {
+
+  const router = useRouter()
+  const logout = async () => {
+    try {
+      await axios.get('/api/users/logout')
+      router.push('/')
+    } catch (error) {
+      console.log(error.message);
+
+    }
+  }
+
   return (
     <div className="flex flex-col items-center justify-center">
       <h1>
@@ -6,6 +22,11 @@ export default function ProfilePage() {
       </h1>
       <hr />
       <p>Profile Page</p>
+      <button
+      className="bg-black text-white p-4 w-40 mt-4"
+      onClick={logout}>
+        Logout
+      </button>
     </div>
   );
 
