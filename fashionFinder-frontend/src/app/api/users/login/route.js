678e538b55e3483a0d261ee5bd78edb62ba17e6b
alexgjeka <alexgjeka0@gmail.com>
2024-02-03 20:07:52 -0500

    Changed token expiration in cookies to 1 day

--- fashionFinder-frontend/src/app/api/users/login/route.js
+++ fashionFinder-frontend/src/app/api/users/login/route.js
@@ -30,7 +30,7 @@ export async function POST(request) {
       email: user.email
     }
     //create token
-    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET, {expiresIn: "1hr"})
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET, {expiresIn: "1d"})
 
     const response = NextResponse.json({
       message: "Login successful",
