d9ae005205e10889199fb2607b95155ba0448f86
alexgjeka <alexgjeka0@gmail.com>
2024-02-03 20:06:52 -0500

    Added event listeners for the enter key

--- fashionFinder-frontend/src/components/buttons/RegisterButton.js
+++ fashionFinder-frontend/src/components/buttons/RegisterButton.js
@@ -1,10 +1,25 @@
-export default function RegisterButton({ onSignup }){
+import { useEffect } from 'react';
+
+export default function RegisterButton({ onSignup }) {
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key === 'Enter') {
+        onSignup();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [onSignup]);
+
   return (
     <button
-    className="bg-black text-white py-4 font-semibold text-center"
-    onClick={onSignup}>
+      className="bg-black text-white py-4 font-semibold text-center"
+      onClick={onSignup}>
       REGISTER
-  </button>
+    </button>
   );
-  
 }
--- fashionFinder-frontend/src/components/buttons/SignInPageButton.js
+++ fashionFinder-frontend/src/components/buttons/SignInPageButton.js
@@ -1,11 +1,25 @@
-'use client';
+import { useEffect } from 'react';
 
 export default function NextButton({ onLogin }) {
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key === 'Enter') {
+        onLogin();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [onLogin]);
+
   return (
-    <button 
-    className="bg-black text-white py-4 px-3 mt-4 mb-4 w-full font-semibold text-center"
-    onClick={onLogin}>
-       NEXT
+    <button
+      className="bg-black text-white py-4 px-3 mt-4 mb-4 w-full font-semibold text-center"
+      onClick={onLogin}>
+      NEXT
     </button>
   );
 }
