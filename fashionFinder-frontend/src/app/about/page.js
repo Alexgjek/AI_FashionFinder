33f818cc98fcad6b31c7fb51a8c8c3ec4c14df41
oloofakalid <oloofakalid27@yahoo.com>
2024-02-05 15:51:33 -0500

    created about page

--- fashionFinder-frontend/src/app/about/page.js
+++ fashionFinder-frontend/src/app/about/page.js
@@ -1,5 +1,19 @@
-export default function AboutPage(){
+export default function AboutPage() {
   return (
-    <div>test</div>
+    <div className="container mx-auto p-4">
+    <h1 className="text-3xl font-bold text-center mb-16">About Page: FashionFinder</h1>
+    <div className="text-2xl text-gray-1500">
+      <p className="mb-10"> 
+  Our AI-powered personal designer utilizes advanced algorithms to analyze user preferences, and style trends to suggest personalized clothing options. We aim to revolutionize the online shopping experience, offering tailored recommendations that align with each user's unique tastes and preferences.
+  </p>
+  <p>
+  At our core, we believe in empowering individuals to express themselves through fashion while providing a seamless and enjoyable shopping journey. With our commitment to innovation and customer satisfaction, we strive to redefine the boundaries of e-commerce and inspire confidence in every purchase.
+  </p>
+  </div>
+  
+      </div>
   );
-}
+  }
+  
+  
+  
