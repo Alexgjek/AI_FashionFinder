f7f68c9818bf99da8a011deed7fedc3a4ab5ad29
alexgjeka <alexgjeka0@gmail.com>
2024-02-04 19:35:30 -0500

    Changed public path logic so logged in users can access '/'

--- fashionFinder-frontend/src/middleware.js
+++ fashionFinder-frontend/src/middleware.js
@@ -2,15 +2,15 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export function middleware(request) {
-  const path = request.nextUrl.pathname
-  const isPublicPath = path === '/login' || path === '/register' || path === '/'
-  const token = request.cookies.get("token")?.value || ''
+  const path = request.nextUrl.pathname;
+  const isPublicPath = path === '/login' || path === '/register' || (path === '/' && !request.cookies.get("token")?.value);
+  const token = request.cookies.get("token")?.value || '';
 
   if (isPublicPath && token){
-    return NextResponse.redirect(new URL('/profile', request.nextUrl))
+    return NextResponse.redirect(new URL('/profile', request.nextUrl));
   }
   if (!isPublicPath && !token){
-    return NextResponse.redirect(new URL('/login', request.nextUrl))
+    return NextResponse.redirect(new URL('/login', request.nextUrl));
   }
 }
 
@@ -22,4 +22,4 @@ export const config = {
     '/register',
     '/albums/:path*'
   ],
-}
+};
