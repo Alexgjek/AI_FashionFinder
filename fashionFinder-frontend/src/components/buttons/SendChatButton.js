d001199c27469c2960fe22071e893415593d18b4
alexgjeka <alexgjeka0@gmail.com>
2024-02-17 16:44:27 -0500

    User input now logged to console and cleared from input box when user presses button or hits enter

--- fashionFinder-frontend/src/components/buttons/SendChatButton.js
+++ fashionFinder-frontend/src/components/buttons/SendChatButton.js
@@ -1,11 +1,34 @@
+import { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
-export default function SendChatButton() {
+export default function SendChatButton({ inputValue, clearInput }) {
+  const handleClick = () => {
+    console.log(inputValue);
+    clearInput();
+  };
+
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        handleClick();
+      }
+    };
+  
+    document.addEventListener('keydown', handleKeyPress);
+  
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [handleClick]);
+
   return (
     <button
       type="button"
-      className="px-4 py-1.5 border border-gray-300 rounded-r-md bg-slate-500 text-white">
+      className="px-4 py-1.5 border border-gray-300 rounded-r-md bg-slate-500 text-white"
+      onClick={handleClick}
+    >
       <FontAwesomeIcon icon={faArrowUp} className="text-white w-5" />
     </button>
   );
