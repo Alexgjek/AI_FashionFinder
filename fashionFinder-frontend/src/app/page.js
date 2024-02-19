f3e0439d1e36e0c7cc7759391ce0a1f085285f79
alexgjeka <alexgjeka0@gmail.com>
2024-02-19 15:35:55 -0500

    When user clicks on the input box it will move to the bottom and the header will move to the top left

--- fashionFinder-frontend/src/app/page.js
+++ fashionFinder-frontend/src/app/page.js
@@ -10,32 +10,17 @@ export default function Home() {
 
   const clearInput = () => setInputValue('');
   
-  const formStyle = isFormFixed
-  ? { 
-      position: 'fixed', 
-      bottom: '3rem', 
-      width: 'calc(100% - 2rem)',
-      left: '1rem', 
-      transition: 'transform 0.3s ease-in-out' 
-    }
-  : { 
-      position: 'static', 
-      width: 'calc(100% - 2rem)',
-      transition: 'transform 0.3s ease-in-out' 
-    };
-
   return (
     <main>
       <section className='p-4 pt-32'>
-        <div>
-          <h1 className='text-6xl font-bold'>
+        <div className={`transition ease-linear duration-800 ${isInputFocused ? 'fixed top-16 left-0 text-xl' : 'static top-0 left-0 text-6xl'}`}>
+          <h1 className='font-bold'>
             Your very own<br /> personal designer
           </h1>
-          <h2 className='text-xl mt-6'>Makes finding outfits so much easier</h2>
+          <h2 className={`text-xl mt-6 ${isInputFocused ? 'hidden' : ''}`}>Makes finding outfits so much easier</h2>
         </div>
         <form
-          className="mt-6 flex shadow-md transition-transform duration-1000 ease-in-out"
-          style={formStyle}
+          className={`mt-6 flex shadow-md ${isFormFixed ? 'fixed bottom-12 left-4 w-11/12 transition-transform duration-1000 ease-in-out transform translate-y-0' : 'static w-11/12 transition-transform duration-1000 ease-in-out transform -translate-y-12'}`}
         >
           <input
             type="text"
@@ -45,7 +30,6 @@ export default function Home() {
               setInputFocused(true);
               setFormFixed(true);
             }}
-            onBlur={() => setInputFocused(false)}
             value={inputValue}
             onChange={e => setInputValue(e.target.value)}
           />
