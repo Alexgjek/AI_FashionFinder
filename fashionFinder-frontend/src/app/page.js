f9102c5f67c57bc3f58072ffddca32cf0bc9aee7
alexgjeka <alexgjeka0@gmail.com>
2024-02-21 15:12:08 -0500

    Changed the overall layout, closley resembles chatgpt UI

--- fashionFinder-frontend/src/app/page.js
+++ fashionFinder-frontend/src/app/page.js
@@ -1,41 +1,44 @@
 'use client';
 import { useState } from 'react';
-import Header from '@/components/Header';
 import SendChatButton from '@/components/buttons/SendChatButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function Home() {
-  const [isInputFocused, setInputFocused] = useState(false);
-  const [isFormFixed, setFormFixed] = useState(false);
+  
   const [inputValue, setInputValue] = useState('');
 
   const clearInput = () => setInputValue('');
   
   return (
-    <main>
-      <section className='p-4 pt-32'>
-        <div className={`transition ease-linear duration-800 ${isInputFocused ? 'fixed top-16 left-0 text-xl' : 'static top-0 left-0 text-6xl'}`}>
-          <h1 className='font-bold'>
-            Your very own<br /> personal designer
-          </h1>
-          <h2 className={`text-xl mt-6 ${isInputFocused ? 'hidden' : ''}`}>Makes finding outfits so much easier</h2>
+    <div className='m-0 w-full h-screen grid grid-cols-7' style={{ height: 'calc(100vh - 60px)' }}>
+      <div className='bg-gray-200 col-span-1 left-0 p-1'>
+      <button className='w-full rounded-lg p-2 hover:bg-gray-100 flex justify-between items-center'>
+        <span className='font-semibold text-sm'>New Chat</span>
+        <FontAwesomeIcon icon={faPenToSquare} />
+      </button>
+      </div>
+      <div className='col-span-6 flex flex-col items-center justify-center'>
+        <div className='mx-auto'>
+        <h1 className='text-4xl mx-auto font-semibold text-center'>
+          Tell us what you're looking for
+        </h1>
+        <h2 className='font-semibold text-xl text-center p-2'>
+          We'll find it for you
+        </h2>
         </div>
-        <form
-          className={`mt-6 flex shadow-md ${isFormFixed ? 'fixed bottom-12 left-4 w-11/12 transition-transform duration-1000 ease-in-out transform translate-y-0' : 'static w-11/12 transition-transform duration-1000 ease-in-out transform -translate-y-12'}`}
-        >
+        <div className="absolute bottom-5 w-full flex justify-center h-16">
           <input
             type="text"
-            placeholder='Message your personal designer...'
-            className="p-2 border border-gray-300 rounded-md block w-full text-sm outline-none"
-            onFocus={() => {
-              setInputFocused(true);
-              setFormFixed(true);
-            }}
             value={inputValue}
             onChange={e => setInputValue(e.target.value)}
+            className="w-2/4 p-4 border rounded-xl outline-none z-10"
+            placeholder="Message FashionFinder..."
           />
           <SendChatButton inputValue={inputValue} clearInput={clearInput}/>
-        </form>
-      </section>
-    </main>
+        </div>
+      </div>
+    </div>
   );
 }
