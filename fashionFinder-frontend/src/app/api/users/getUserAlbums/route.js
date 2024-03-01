bac947955b13a65e7bc9f8e7eebd4ab6d3555aeb
alexgjeka <alexgjeka0@gmail.com>
2024-02-29 19:55:50 -0500

    api for grabbing the users albums from the db

---
+++ fashionFinder-frontend/src/app/api/users/getUserAlbums/route.js
@@ -0,0 +1,30 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+connect();
+
+export async function GET(request) {
+  const token = request.cookies.get("token")?.value || '';
+  const decodedToken = jwt.decode(token);
+  const email = decodedToken ? decodedToken.email : '';
+
+  try {
+    const userEmail = email
+
+    if (!userEmail) {
+      return NextResponse.json({ error: "No email" }, { status: 400 });
+    }
+
+    const user = await User.findOne({ email: userEmail });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ albums: user.albums });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
