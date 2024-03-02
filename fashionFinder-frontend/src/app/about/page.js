2f6c766d2cfc840390a0af3735797a67a84fb0ec
alexgjeka <alexgjeka0@gmail.com>
2024-03-02 15:20:59 -0500

    Added logo to about page

--- fashionFinder-frontend/src/app/about/page.js
+++ fashionFinder-frontend/src/app/about/page.js
@@ -1,5 +1,16 @@
-export default function AboutPage(){
+export default function AboutPage() {
   return (
-    <div>test</div>
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold text-center mb-16">About FashionFinder</h1>
+      <img src="/FFlogo.png" className="mx-auto"/>
+      <div className="text-2xl text-gray-1500">
+        <p className="mt-0"> 
+          Our AI-powered personal designer utilizes advanced algorithms to analyze user preferences. We aim to revolutionize the online shopping experience, offering tailored recommendations that align with each user's unique tastes and preferences.
+        </p>
+      </div>
+    </div>
   );
 }
+  
+  
+  
