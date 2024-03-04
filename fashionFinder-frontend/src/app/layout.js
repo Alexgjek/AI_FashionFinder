579f00b04c14bb9570062bde197f4ff48b74690a
alexgjeka <alexgjeka0@gmail.com>
2024-03-03 20:39:24 -0500

    fixing conflicts

--- fashionFinder-frontend/src/app/layout.js
+++ fashionFinder-frontend/src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <main>
           <Header />
-          <div>
+          <div className="max-w-4xl mx-auto p-6">
           {children}
           </div>
         </main>
