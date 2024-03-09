4d1eab65176ff371e60b9317451a0f773a9a82d8
alexgjeka <alexgjeka0@gmail.com>
2024-03-09 00:24:34 -0500

    search bar component

--- fashionFinder-frontend/src/components/sorting/albumSorting/AlbumSort.js
+++ fashionFinder-frontend/src/components/sorting/albumSorting/AlbumSort.js
@@ -9,12 +9,12 @@ export default function AlbumSort({ albums, onSortChange }) {
   };
 
   return (
-    <div className='mt-2 ml-5 inline-flex shadow-sm'>
+    <div className='mt-3 ml-5 inline-flex shadow-md border border-gray-300'>
       <select className='outline-none' value={sortType} onChange={(e) => handleSortChange(e.target.value)}>
         <option value="">Sort by</option>
         <option value="alphabetical">Alphabetical</option>
-        <option value="ascending">Newest to Oldest</option>
-        <option value="descending">Oldest to Newest</option>
+        <option value="descending">Newest to Oldest</option>
+        <option value="ascending">Oldest to Newest</option>
       </select>
     </div>
   );
