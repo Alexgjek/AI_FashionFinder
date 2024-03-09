f80c092efd43e3e5caf9b9211c6687d6cf0f5367
alexgjeka <alexgjeka0@gmail.com>
2024-03-08 20:25:06 -0500

    landing page

--- fashionFinder-frontend/src/app/page.js
+++ fashionFinder-frontend/src/app/page.js
@@ -1,125 +1,74 @@
-
 'use client';
-import { useState, useRef, useEffect } from 'react';
-import SendChatButton from '@/components/buttons/SendChatButton';
+import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
-import axios from 'axios';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import { useRouter } from 'next/navigation'; 
 
-export default function Home() {
-  const [inputValue, setInputValue] = useState('');
-  const [conversation, setConversation] = useState([]);
-  const [isSubmitted, setIsSubmitted] = useState(false);
-  const conversationContainerRef = useRef(null);
-
-  const clearInput = () => {
-    setInputValue('');
-  };
-
-  const handleNewChat = () => {
-    setConversation([]);
-    setIsSubmitted(false);
-  };
+export default function LandingPage() {
+  const [phraseIndex, setPhraseIndex] = useState(0);
+  const [fadeIn, setFadeIn] = useState(true);
+  const router = useRouter();
 
   useEffect(() => {
-    if (conversationContainerRef.current) {
-      conversationContainerRef.current.scrollTop = conversationContainerRef.current.scrollHeight;
-    }
-  }, [conversation]);
+    const interval = setInterval(() => {
+      setFadeIn(false);
+      setTimeout(() => {
+        setPhraseIndex((prevIndex) => (prevIndex + 1) % userSearchExamples.length);
+        setFadeIn(true);
+      }, 500); 
+    }, 5000); 
+    return () => clearInterval(interval);
+  }, [phraseIndex]);
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    console.log(inputValue)
-
-    setIsSubmitted(true);
-    const newMessage = {
-      sender: "You",
-      message: `${inputValue}`,
-      link: false
-    };
-    setConversation(prevConversation => [...prevConversation, newMessage]);
-    clearInput();
-
-    try {
-      const response = await axios.post('http://localhost:8000/getAIResponse', {
-        prompt: inputValue,
-      });
-      const respJson = response.data;
-
-      const fashionFinderMessage = {
-        sender: "FashionFinder",
-        message: respJson.ai_response,
-        link: false
-      };
-
-      setConversation(prevConversation => [...prevConversation, fashionFinderMessage]);
-    } catch (error) {
-      console.error('Error fetching AI response:', error);
-    }
+  const handleLoginClick = () => {
+    router.push('/login');
   };
 
+  const handleRegisterClick = () => {
+    router.push('/register');
+  }; 
 
-  function handleKeyDown(e) {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      handleSubmit(e);
-    }
-  }
-
+  const userSearchExamples = [
+    "Find me an outfit for a night out",
+    "What would go well with a grey hoodie?",
+    "I need a dress for a wedding",
+    "I need a casual outfit for a date",
+    "What should I wear to a job interview?",
+    "Help me find an outfit for a vacation"
+  ];
 
   return (
-    <div className='m-0 w-full h-screen grid grid-cols-7' style={{ height: 'calc(100vh - 60px)' }}>
-      <div className='bg-gray-200 col-span-1 left-0 p-1'>
-        <button
-          className='w-full rounded-lg p-2 hover:bg-gray-100 flex justify-between items-center'
-          onClick={handleNewChat}
-        >
-          <span className='font-semibold text-sm'>New Chat</span>
-          <FontAwesomeIcon icon={faPenToSquare} />
-        </button>
-      </div>
-      <div className='col-span-6 flex flex-col items-center justify-center'>
-        {isSubmitted ? (
-          <div
-            ref={conversationContainerRef}
-            className='absolute top-20 w-5/6 p-2'
-            style={{ maxHeight: 'calc(100vh - 200px)', overflowY: 'auto' }}
-          >
-            {conversation.map((conv, index) => (
-              <div key={index} className='p-2'>
-                <>
-                  <p className='font-bold'>{conv.sender}</p>
-                  <div className='font-md'>{conv.message.split("\n").map((part, idx) => conv.link ? (<a key={idx} href={part} target="_blank" style={{ display: "block", color: "blue", textDecoration: "underline" }}>{part}</a>) : (<p key={idx}>{part}</p>))}</div>
-                  <hr className='bg-black' />
-                </>
-              </div>
-            ))}
+    <main>
+      <div className="w-full h-screen grid grid-cols-5">
+        <div className="col-span-3 bg-customColor">
+          <p className="text-purple-400 p-1 font-bold inline-flex gap-2 text-2xl items-center absolute left-5 top-5">
+            FashionFinder
+            <FontAwesomeIcon icon={faCircle} className="w-5" />
+          </p>
+          <div className="h-full flex items-center">
+            <p className={`typewriter text-purple-400 text-4xl font-semibold text-left ml-4 mr-5 ${fadeIn ? 'fade-in' : 'fade-out'}`} key={phraseIndex}>
+              {userSearchExamples[phraseIndex]}
+            </p>
           </div>
-        ) : (
-            <div className='mx-auto'>
-              <img src="/FFlogo.png" />
-              <h1 className='text-4xl mx-auto font-semibold text-center'>
-                Tell us what you're looking for
-              </h1>
-              <h2 className='font-semibold text-xl text-center p-2 mb-20'>
-                We'll find it for you
-              </h2>
-            </div>
-          )}
-        <div className="absolute bottom-5 w-5/6 flex justify-center h-16" >
-          <input
-            type="text"
-            value={inputValue}
-            onKeyDown={handleKeyDown}
-            onChange={e => setInputValue(e.target.value)}
-            className="w-2/4 p-4 border rounded-xl outline-none z-10"
-            placeholder="Message FashionFinder..."
-          />
-          <SendChatButton inputValue={inputValue} clearInput={clearInput} handleClick={handleSubmit} />
         </div>
-
+        <div className="col-span-2 bg-black flex flex-col items-center justify-center">
+          <p className="text-3xl font-bold text-white">
+            Get started
+          </p>
+          <div className='flex gap-4 w-full p-6'>
+            <button
+              className="p-4 bg-blue-700 font-bold w-full rounded-lg shadow-xl text-white"
+              onClick={handleLoginClick}>
+              Login
+            </button>
+            <button
+              className='p-4 bg-blue-700 font-bold w-full rounded-lg shadow-xl text-white'
+              onClick={handleRegisterClick}>
+              Register
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    </main>
+  )
 }
