244a42fa9e9f7f28e5aa8891a39df8a95754e3f1
alexgjeka <alexgjeka0@gmail.com>
2024-03-10 01:28:13 -0500

    wrapped everyhting in main element and added padding

--- fashionFinder-frontend/src/app/login/page.js
+++ fashionFinder-frontend/src/app/login/page.js
@@ -60,74 +60,76 @@ export default function LoginPage() {
   return (
     <main>
       <ShortHeader />
-      <div className="max-w-lg mx-auto">
-        <h1 className="text-2xl font-bold mt-10 mb-5">Enter your email</h1>
-        {error && <p className='text-red-500 text-sm mb-2'>{error}</p>}
-        <input
-          className="border border-black py-4 px-3 outline-none w-full mb-2"
-          type="text"
-          id="email"
-          placeholder="E-mail address"
-          value={user.email}
-          onChange={(e) => {
-            setUser({ ...user, email: e.target.value.toLowerCase()});
-            setError("");
-          }}
-        />
-        <div className="relative">
+      <div className="px-2">
+        <div className="max-w-lg mx-auto">
+          <h1 className="text-2xl font-bold mt-10 mb-5">Enter your email</h1>
+          {error && <p className='text-red-500 text-sm mb-2'>{error}</p>}
           <input
-            className="border border-black py-4 px-3 outline-none w-full"
-            type={passwordVisible ? "text" : "password"}
-            id="password"
-            placeholder="Password"
-            value={user.password}
+            className="border border-black py-4 px-3 outline-none w-full mb-2"
+            type="text"
+            id="email"
+            placeholder="E-mail address"
+            value={user.email}
             onChange={(e) => {
-              setUser({ ...user, password: e.target.value });
+              setUser({ ...user, email: e.target.value.toLowerCase()});
               setError("");
             }}
           />
-          <img
-            src={passwordVisible ? "/eye.png" : "/eyeslash.png"}
-            width="24px"
-            height="24px"
-            style={{ display: 'inline', marginLeft: '-30px', cursor: 'pointer', position: 'absolute', top: '50%', transform: 'translateY(-50%)' }}
-            onClick={() => setPasswordVisible(!passwordVisible)}
-          />
-        </div>
-        <div className="flex items-center justify-between mb-2">
-          <div className="flex items-center">
+          <div className="relative">
             <input
-              type="checkbox"
-              id="rememberMe"
-              checked={user.rememberMe}
-              onChange={(e) => setUser({ ...user, rememberMe: e.target.checked })}
+              className="border border-black py-4 px-3 outline-none w-full"
+              type={passwordVisible ? "text" : "password"}
+              id="password"
+              placeholder="Password"
+              value={user.password}
+              onChange={(e) => {
+                setUser({ ...user, password: e.target.value });
+                setError("");
+              }}
             />
-            <label htmlFor="rememberMe" className="ml-2">Remember me</label>
+            <img
+              src={passwordVisible ? "/eye.png" : "/eyeslash.png"}
+              width="24px"
+              height="24px"
+              style={{ display: 'inline', marginLeft: '-30px', cursor: 'pointer', position: 'absolute', top: '50%', transform: 'translateY(-50%)' }}
+              onClick={() => setPasswordVisible(!passwordVisible)}
+            />
           </div>
-          <div>
-            <Link href={'/forgot'} className='hover:underline hover:text-purple-800'>Forgot Password?</Link>
+          <div className="flex items-center justify-between mb-2">
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                id="rememberMe"
+                checked={user.rememberMe}
+                onChange={(e) => setUser({ ...user, rememberMe: e.target.checked })}
+              />
+              <label htmlFor="rememberMe" className="ml-2">Remember me</label>
+            </div>
+            <div>
+              <Link href={'/forgot'} className='hover:underline hover:text-purple-800'>Forgot Password?</Link>
+            </div>
           </div>
-        </div>
-        <NextButton onLogin={onLogin}/>
-        <div className='flex gap-1 text-sm py-2'>
-          <span>Don't have an account?</span>
-          <Link href={'/register'} className='hover:underline hover:text-purple-800'>Create account</Link>
-        </div>
-        <hr className='border-gray-300 mt-4 mb-4'/>
-        <span className="text-sm">Make your experience even more fun when signing in</span>
+          <NextButton onLogin={onLogin}/>
+          <div className='flex gap-1 text-sm py-2'>
+            <span>Don't have an account?</span>
+            <Link href={'/register'} className='hover:underline hover:text-purple-800'>Create account</Link>
+          </div>
+          <hr className='border-gray-300 mt-4 mb-4'/>
+          <span className="text-sm">Make your experience even more fun when signing in</span>
 
-        <div className="p-5 text-xs space-y-3">
-          <div className="flex gap-4 items-center">
-            <FontAwesomeIcon icon={faHeart} className="w-5 h-5" />
-            <p>Save outfits to albums you create</p>
-          </div>
-          <div className="flex gap-4 items-center">
-            <FontAwesomeIcon icon={faShirt} className="w-5 h-5" />
-            <p>Set your brand preferences</p>
-          </div>
-          <div className="flex gap-4 items-center">
-            <FontAwesomeIcon icon={faSackDollar} className="w-5 h-5" />
-            <p>Set your budget</p>
+          <div className="p-5 text-xs space-y-3">
+            <div className="flex gap-4 items-center">
+              <FontAwesomeIcon icon={faHeart} className="w-5 h-5" />
+              <p>Save outfits to albums you create</p>
+            </div>
+            <div className="flex gap-4 items-center">
+              <FontAwesomeIcon icon={faShirt} className="w-5 h-5" />
+              <p>Set your brand preferences</p>
+            </div>
+            <div className="flex gap-4 items-center">
+              <FontAwesomeIcon icon={faSackDollar} className="w-5 h-5" />
+              <p>Set your budget</p>
+            </div>
           </div>
         </div>
       </div>
