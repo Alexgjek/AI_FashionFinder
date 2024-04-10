3a88f85fcf601b8e92f813f825fe012e97727bec
Angjelo Mana <hc7676@wayne.edu>
2024-04-10 18:46:47 -0400

    profile page brand search and filtering

--- fashionFinder-frontend/src/app/profile/page.js
+++ fashionFinder-frontend/src/app/profile/page.js
@@ -4,73 +4,79 @@ import { useRouter } from 'next/navigation';
 import { useState, useEffect } from 'react';
 import Header from '@/components/Header';
 
-
 export default function ProfilePage() {
   const router = useRouter();
   const [isEditing, setIsEditing] = useState(false);
   const [brandError, setBrandError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState('');
   const [disabledInputs, setDisabledInputs] = useState([]);
   const [userInfo, setUserInfo] = useState({
     email: "",
     password: "",
     brands: [],
-    budget: 100, // Default value for budget is $100
+    budget: '',
   });
 
+  
+  const [brandList, setBrandList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedBrand, setSelectedBrand] = useState('');
+  const [filteredBrands, setFilteredBrands] = useState([]);
 
   useEffect(() => {
-    async function fetchUserData() {
-      try {
-        const response = await axios.get("/api/users/userInfo");
-        setUserInfo(response.data);
-        setDisabledInputs(response.data.brands.map(() => true));
-      } catch (error) {
-        console.error("Error fetching user data:", error);
+      async function fetchData() {
+          try {
+              const userDataResponse = await axios.get("/api/users/userInfo");
+              setUserInfo(userDataResponse.data);
+              setDisabledInputs(userDataResponse.data.brands.map(() => true));
+
+              const brandsResponse = await axios.get("/api/users/getBrands");
+              setBrandList(brandsResponse.data.brands);
+          } catch (error) {
+              console.error("Error fetching data:", error);
+          }
       }
-    }
 
-
-    fetchUserData();
+      fetchData();
   }, []);
 
+  useEffect(() => {
+      // Filter brands based on the search term
+      const filtered = brandList.filter(brand =>
+          brand.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredBrands(filtered);
+  }, [searchTerm, brandList]);
+
+  const handleSelectChange = (e) => {
+      setSelectedBrand(e.target.value);
+  };
 
   const handleSubmit = async () => {
-    const filteredBrands = userInfo.brands.filter(
-      (brand) => brand.trim() !== ""
-    );
-
+    const filteredBrands = userInfo.brands.filter(brand => brand.trim() !== '');
 
     if (filteredBrands.length === 0 && !isEditing) {
-      setIsEditing(true);
-      setBrandError(true);
+      setIsEditing(true); 
+      setBrandError(true); 
       return;
     } else {
-      setBrandError(false);
+      setBrandError(false); 
     }
 
-
-    const uniqueBrands = [
-      ...new Set(filteredBrands.map((brand) => brand.trim())),
-    ];
+    const uniqueBrands = [...new Set(filteredBrands.map(brand => brand.trim()))];
     if (uniqueBrands.length !== filteredBrands.length) {
       setErrorMessage("Duplicate brand");
       return;
     }
 
-
     try {
-      const response = await axios.post("/api/users/setBrandOrBudget", {
-        ...userInfo,
-        brands: uniqueBrands,
-      });
+      const response = await axios.post("/api/users/setBrandOrBudget", { ...userInfo, brands: uniqueBrands });
       console.log(response.data);
       setIsEditing(false);
-      setUserInfo((prevUserInfo) => ({
+      setUserInfo(prevUserInfo => ({
         ...prevUserInfo,
-        brands: uniqueBrands,
+        brands: uniqueBrands 
       }));
-
 
       setDisabledInputs(uniqueBrands.map(() => true));
     } catch (error) {
@@ -86,104 +92,86 @@ export default function ProfilePage() {
     });
   };
 
-
   const handleBudgetChange = (value) => {
-    const budgetValue = parseInt(value);
-    handleChange("budget", budgetValue === 0 ? 0 : budgetValue || ""); // Set budget to 0 if value is explicitly set to 0, otherwise set it to the parsed value or an empty string
+    const formattedValue = value.replace(/^0(?!\.)/, '');
+    handleChange("budget", formattedValue);
   };
 
+  const handleBudgetReset = async () => {
+    try {
+      await axios.delete("/api/users/resetBudget");
+      setUserInfo(prevUserInfo => ({
+        ...prevUserInfo,
+        budget: ''
+      }));
+      console.log("Budget reset successfully");
+      setErrorMessage('');
+    } catch (error) {
+      console.error("Error resetting budget:", error);
+    }
+  };
 
   const addBrand = () => {
-    const isEmptyField = userInfo.brands.some((brand) => brand.trim() === "");
-    if (isEmptyField) {
-      setBrandError(true);
-      return;
+    if (selectedBrand.trim() === "") {
+        setBrandError(true);
+        return;
     } else {
-      setBrandError(false);
+        setBrandError(false);
     }
 
-
-    setUserInfo({
-      ...userInfo,
-      brands: [...userInfo.brands, ""],
-    });
-    setDisabledInputs((prevDisabledInputs) => [...prevDisabledInputs, false]);
-  };
-
-
-  const handleBrandChange = (index, value) => {
-    const capitalizedBrand = value
-      .split(" ")
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-      .join(" ");
-    const updatedBrands = [...userInfo.brands];
-    updatedBrands[index] = capitalizedBrand;
-    setUserInfo({
-      ...userInfo,
-      brands: updatedBrands,
-    });
-
-
-    if (!isEditing) {
-      setDisabledInputs((prevDisabledInputs) => {
-        const updatedDisabledInputs = [...prevDisabledInputs];
-        updatedDisabledInputs[index] = false;
-        return updatedDisabledInputs;
-      });
+    // Check if the selected brand already exists in user's brands
+    if (userInfo.brands.includes(selectedBrand.trim())) {
+        setErrorMessage("Brand already added.");
+        return;
     }
-  };
 
+    setUserInfo({
+        ...userInfo,
+        brands: [...userInfo.brands, selectedBrand.trim()],
+    });
+    setDisabledInputs(prevDisabledInputs => [...prevDisabledInputs, false]);
+    setSelectedBrand('');
+};
 
   const handleDeleteBrand = async (index) => {
     try {
       const deletedBrand = userInfo.brands[index];
-      const response = await axios.delete(
-        `/api/users/deleteBrands?brand=${deletedBrand}`
-      );
+      const response = await axios.delete(`/api/users/deleteBrands?brand=${deletedBrand}`);
       if (response.data.success) {
-        setUserInfo((prevUserInfo) => ({
+        setUserInfo(prevUserInfo => ({
           ...prevUserInfo,
-          brands: [
-            ...prevUserInfo.brands.slice(0, index),
-            ...prevUserInfo.brands.slice(index + 1),
-          ],
+          brands: [...prevUserInfo.brands.slice(0, index), ...prevUserInfo.brands.slice(index + 1)]
         }));
 
-
-        setDisabledInputs((prevDisabledInputs) => {
+        setDisabledInputs(prevDisabledInputs => {
           const updatedDisabledInputs = [...prevDisabledInputs];
-          updatedDisabledInputs.splice(index, 1);
+          updatedDisabledInputs.splice(index, 1); 
           return updatedDisabledInputs;
         });
-
 
         console.log("Brand removed successfully");
       } else {
         console.log("Error removing brand:", response.data.error);
       }
-      setErrorMessage("");
+      setErrorMessage('');
     } catch (error) {
       console.error("Error removing brand:", error);
     }
-  };
-
+  }
 
   const changePassword = () => {
     router.push("/forgot");
   };
-  
-
 
   return (
     <main>
-      <Header/>
-    <div className="p-6">
+    <Header/>
+    <div className='p-6'>
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-3xl font-bold p-3">Personal Information</h1>
         <div className="flex flex-col items-start">
           <h2 className="text-gray-600">
-            Manage your personal details, brand preferences, and budget
-            preferences
+            Manage your personal details, brand preferences, and budget preferences
           </h2>
           <div className="w-full font-semibold mt-4">
             <hr className="border border-gray-300 mt-2 mb-2" />
@@ -197,8 +185,10 @@ export default function ProfilePage() {
             <hr className="border border-gray-300 mt-2 mb-2" />
             <h4 className="mb-4">
               E-MAIL
-              <br />
-              <span className="font-normal">{userInfo.email}</span>
+              <br/>
+              <span className='font-normal'>
+                {userInfo.email}
+              </span>
             </h4>
             <hr className="border border-gray-300 mt-2 mb-2" />
             <div>
@@ -206,14 +196,15 @@ export default function ProfilePage() {
                 <div className="flex justify-between">
                   <div>
                     PASSWORD
-                    <br />
-                    <span className="font-normal">{"**********"} </span>
+                    <br/>
+                    <span className='font-normal'>
+                      {"**********"}{" "}
+                    </span>
                   </div>
                   {isEditing && (
                     <button
                       onClick={changePassword}
-                      className="bg-transparent text-black px-1 py-1 rounded-md font-normal hover:opacity-50"
-                    >
+                      className="bg-transparent text-black px-1 py-1 rounded-md font-normal hover:opacity-50">
                       Change Password
                     </button>
                   )}
@@ -222,78 +213,89 @@ export default function ProfilePage() {
               <hr className="border border-gray-300 mt-2 mb-2" />
             </div>
             <h6 className="mb-4">
-              BRANDS{" "}
-              {brandError && (
-                <p className="text-red-500 font-normal">Field must be filled</p>
-              )}
-              {errorMessage && (
-                <p className="text-red-500 font-normal">{errorMessage}</p>
-              )}
-              <div>
-                {isEditing ? (
-                  <>
-                    {userInfo.brands &&
-                      userInfo.brands.map((brand, index) => (
-                        <div
-                          key={index}
-                          style={{ display: "flex", alignItems: "center" }}
-                        >
-                          <input
-                            type="text"
-                            value={brand}
-                            onChange={(e) =>
-                              handleBrandChange(index, e.target.value)
-                            }
-                            placeholder="Enter brand"
-                            className="border border-gray-300 p-2 rounded-md mr-2 outline-none"
-                            disabled={disabledInputs[index]}
-                          />
-                          {brand && (
-                            <button
-                              onClick={() => handleDeleteBrand(index)}
-                              className="bg-gray-500 text-white px-2 py-1 rounded-md"
-                            >
-                              Remove
-                            </button>
-                          )}
+                        BRANDS{" "}
+                        <div>
+                            {isEditing ? (
+    <>
+        <input
+    type="text"
+    placeholder="Search brands..."
+    value={searchTerm}
+    onChange={e => setSearchTerm(e.target.value)}
+    className="border border-gray-300 mr-2 p-2 rounded-md outline-none w-48" // Fixed width added
+/>
+<select
+    className="border border-gray-300 p-2 rounded-md outline-none w-48" // Fixed width added
+    onChange={handleSelectChange}
+    value={selectedBrand}
+    onClick={() => setErrorMessage('')} // Clear error message on click
+>
+    <option value="">Select a brand...</option>
+    {filteredBrands.map((brand, index) => (
+        <option key={index} value={brand}>{brand}</option>
+    ))}
+</select>
+        <button
+            onClick={addBrand}
+            className="bg-black text-white m-2 px-2 py-1 rounded-md mt-2"
+        >
+            Add Brand
+        </button>
+        {errorMessage && (
+            <p className='text-red-500 font-normal'>{errorMessage}</p>
+        )}
+    </>
+) : (
+    <span className="font-normal">
+        {userInfo.brands.filter(brand => brand.trim() !== "").join(", ")}
+    </span>
+)}
+                            {brandError && (
+                                <p className='text-red-500 font-normal'>Brand field must be filled</p>
+                            )}
                         </div>
-                      ))}
+                    </h6>
+            {isEditing && (
+              <>
+                {userInfo.brands.map((brand, index) => (
+                  <div key={index} className="flex justify-between items-center mt-2">
+                    <span>{brand}</span>
                     <button
-                      onClick={addBrand}
-                      className="bg-black text-white px-2 py-1 rounded-md mt-2"
+                      onClick={() => handleDeleteBrand(index)}
+                      className="bg-red-500 text-white px-2 py-1 rounded-md"
                     >
-                      Add Brand
+                      Remove
                     </button>
-                  </>
-                ) : (
-                  <span className="font-normal">
-                    {userInfo.brands &&
-                      userInfo.brands.filter(Boolean).join(", ")}
-                  </span>
-                )}
-              </div>
-            </h6>
+                  </div>
+                ))}
+              </>
+            )}
             <hr className="border border-gray-300 mt-2 mb-2" />
             <h6 className="mb-4">
               BUDGET{" "}
               <div>
                 {isEditing ? (
-                  <div className="flex justify-between items-center">
+                  <div className='flex justify-between items-center'>
                     <input
-                      type="range"
-                      min={100}
-                      max={10000}
+                      type="text"
                       value={userInfo.budget}
                       onChange={(e) => handleBudgetChange(e.target.value)}
-                      className="border border-black rounded-md outline-none appearance-none bg-white thumb-black"
+                      placeholder="e.g. $1,000"
+                      className="border border-gray-300 p-2 rounded-md outline-none"
                     />
-                    {userInfo.budget !== undefined && (
-                      <span className="font-normal">{`$${userInfo.budget}`}</span>
-                    )}
+                    <button
+                      onClick={handleBudgetReset}
+                      className='font-normal hover:opacity-50'>
+                      Reset Budget 
+                    </button>
                   </div>
-                ) : userInfo.budget !== undefined && userInfo.budget !== "" ? (
-                  <span className="font-normal">{`$${userInfo.budget}`}</span>
-                ) : null}
+                ) : userInfo.budget !== undefined && userInfo.budget !== '' ? (
+                  <span className="font-normal">
+                    {`$${userInfo.budget}`}
+                  </span>
+                ) : (
+                  ""
+                )}
               </div>
             </h6>
           </div>
@@ -302,14 +304,12 @@ export default function ProfilePage() {
           className="bg-black text-white p-4 w-40 mt-4"
           onClick={isEditing ? handleSubmit : () => setIsEditing(true)}
         >
-          {isEditing ? (
-            <p className="font-semibold">Save</p>
-          ) : (
-            <p className="font-semibold">Edit</p>
-          )}
+          {isEditing ? 
+          <p className='font-semibold'>Save</p> : 
+          <p className='font-semibold'>Edit</p>}
         </button>
       </div>
-      </div>
-    </main>
+    </div>
+  </main>
   );
 }
